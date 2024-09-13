b29d510fc2d4a74b88b03b0ae12e0873a44e0be5
yunac <yunac.owob@gmail.com>
2024-09-13 14:37:40 +0800

    chore: init

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Yuna Chou on 2024/9/13.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
