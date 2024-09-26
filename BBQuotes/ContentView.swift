ea3a84910a20cda830c1548d8602e78edaebd449
yunac <yunac.owob@gmail.com>
2024-09-26 15:15:29 +0800

    feat(views): add QuoteView and implement tab-based navigation
    - Create QuoteView.swift file for displaying quotes
    - Update ContentView to use TabView with Breaking Bad and Better Call
    Saul tabs
    - Set dark color scheme for the app

--- BBQuotes/ContentView.swift
+++ BBQuotes/ContentView.swift
@@ -5,17 +5,28 @@
 //  Created by Yuna Chou on 2024/9/13.
 //
 
+import Inject
 import SwiftUI
 
 struct ContentView: View {
+    @ObserveInjection var injection
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            QuoteView(production: .breakingBad)
+                .toolbarBackground(.visible, for: .tabBar)
+                .tabItem {
+                    Label("Breaking Bad", systemImage: "tortoise")
+                }
+
+            QuoteView(production: .betterCallSaul)
+                .toolbarBackground(.visible, for: .tabBar)
+                .tabItem {
+                    Label("Better Call Saul", systemImage: "briefcase")
+                }
         }
-        .padding()
+        .preferredColorScheme(.dark)
+        .enableInjection()
     }
 }
 
---
+++ BBQuotes/QuoteView.swift
@@ -0,0 +1,108 @@
+//
+//  QuoteView.swift
+//  BBQuotes
+//
+//  Created by Yuna Chou on 2024/9/23.
+//
+
+import Inject
+import SwiftUI
+
+struct QuoteView: View {
+    let production: Production
+    private let viewModel = ViewModel()
+    private let backgroundImgName: String
+    private let buttonColor: Color
+    private let shadowColor: Color
+
+    init(production: Production) {
+        self.production = production
+        backgroundImgName = production.rawValue.replacingOccurrences(of: " ", with: "-").lowercased()
+        let colorName = production.rawValue.replacingOccurrences(of: " ", with: "")
+        buttonColor = Color("\(colorName)Button")
+        shadowColor = Color("\(colorName)Shadow")
+
+        Task { [self] in
+            await viewModel.fetchData(for: production)
+        }
+    }
+
+    var body: some View {
+        GeometryReader { geo in
+            ZStack {
+                Image(backgroundImgName)
+                    .resizable()
+                    .frame(width: geo.size.width * 2.7, height: geo.size.height * 1.2)
+
+                VStack {
+                    VStack {
+                        Spacer(minLength: 60)
+
+                        switch viewModel.fetchStatus {
+                        case .idle:
+                            EmptyView()
+                        case .fetching:
+                            ProgressView()
+                        case .success:
+                            Text(viewModel.quote!.quote)
+                                .minimumScaleFactor(0.5)
+                                .foregroundStyle(.white)
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .background(.black.opacity(0.5))
+                                .clipShape(.rect(cornerRadius: 25))
+                                .padding(.horizontal)
+
+                            ZStack(alignment: .bottom) {
+                                AsyncImage(url: viewModel.character!.images.randomElement()) { img in
+                                    img
+                                        .resizable()
+                                        .scaledToFill()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                .frame(width: geo.size.width / 1.1, height: geo.size.height / 1.8)
+
+                                Text(viewModel.character!.name)
+                                    .foregroundStyle(.white)
+                                    .padding(10)
+                                    .frame(maxWidth: .infinity)
+                                    .background(.ultraThinMaterial)
+                            }
+                            .frame(width: geo.size.width / 1.1, height: geo.size.height / 1.8)
+                            .clipShape(.rect(cornerRadius: 50))
+                        case let .failure(error):
+                            Text(error.localizedDescription)
+                        }
+
+                        Spacer()
+                    }
+
+                    Button {
+                        Task {
+                            await viewModel.fetchData(for: production)
+                        }
+                    } label: {
+                        Text("Get Random Quotes")
+                            .font(.title3)
+                            .foregroundStyle(.white)
+                            .padding()
+                            .background(buttonColor)
+                            .clipShape(.rect(cornerRadius: 7))
+                            .shadow(color: shadowColor, radius: 5)
+                    }
+
+                    Spacer(minLength: 95)
+                }
+                .frame(width: geo.size.width, height: geo.size.height)
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    QuoteView(production: .breakingBad)
+        .preferredColorScheme(.dark)
+}
